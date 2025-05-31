6d9cdb5a97a168c42a361abd0cefd86c33027663
Priyanshu Barman <priyanshubarman741@gmail.com>
2025-05-31 22:45:35 +0530

    first commit

---
+++ old-way/script.js
@@ -0,0 +1,27 @@
+lucide.createIcons();
+
+const form = document.querySelector("form");
+const modal = document.querySelector(".modal");
+const closeModalBtn = document.querySelector(".close-modal-btn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  modal.style.display = "block";
+  modal.style.backdropFilter = "blur(16px)";
+  modal.style.background = "rgba(0, 0, 0, 0.1)";
+
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const phone = document.querySelector("#phone").value;
+  const password = document.querySelector("#password").value;
+
+  document.querySelector("#show-name").innerHTML = name;
+  document.querySelector("#show-email").innerHTML = email;
+  document.querySelector("#show-phone").innerHTML = phone;
+  document.querySelector("#show-password").innerHTML = password;
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
